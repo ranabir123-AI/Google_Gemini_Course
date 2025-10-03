02f41febfc69c5a154437951fe1fd85994f2f637
ranabir123-AI <ranabir.123@gmail.com>
2025-10-03 16:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ sys.modules['pysqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from streamlit import spinner
-from streamlit.web.server.server import server_port_is_manually_set
+
 
 from supporting_functions import (
      extract_video_id,
@@ -103,3 +103,4 @@ if task_option=="Chat with Video" and "vector_store" in st.session_state:
            st.write(response)
 
         st.session_state.messages.append({'role': 'assistant', 'content':response})
+
